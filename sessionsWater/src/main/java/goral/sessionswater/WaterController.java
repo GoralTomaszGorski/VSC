2113859ecda2a508d16a4989a13fbbc255d1760a
tomasz <tomasz.gorski@gmail.com>
2023-10-23 22:35:50 +0200

    session exe WaterController

---
+++ sessionsWater/src/main/java/goral/sessionswater/WaterController.java
@@ -0,0 +1,46 @@
+package goral.sessionswater;//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet({"/control"})
+public class WaterController extends HttpServlet {
+    private static final int GLASS_OF_WATER = 250;
+
+    public WaterController() {
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        String option = request.getParameter("option");
+        HttpSession session = request.getSession();
+        if (option.equals("water")) {
+            this.increaseWaterLevel(session);
+        } else if (option.equals("clear")) {
+            session.removeAttribute("water");
+        }
+
+        request.getRequestDispatcher("/index.jsp").forward(request, response);
+    }
+
+    private void increaseWaterLevel(HttpSession session) {
+        Object water = session.getAttribute("water");
+        int resultWater;
+        if (water == null) {
+            resultWater = 250;
+        } else {
+            resultWater = (Integer)water + 250;
+        }
+
+        session.setAttribute("water", resultWater);
+    }
+}
