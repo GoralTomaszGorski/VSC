1f1c0ea9d92e6fdbf03a34f20eafe6821c7ce76d
Tomasz GÓRSKI <98718498+GoralTomaszGorski@users.noreply.github.com>
2023-11-13 21:13:56 +0100

    Add files via upload

---
+++ officePsychotherapist/src/main/java/goral/officepsychotherapist/OfficePsychotherapistApplication.java
@@ -0,0 +1,13 @@
+package goral.officepsychotherapist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficePsychotherapistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OfficePsychotherapistApplication.class, args);
+    }
+
+}
---
+++ officePsychotherapist/src/test/java/goral/officepsychotherapist/OfficePsychotherapistApplicationTests.java
@@ -0,0 +1,13 @@
+package goral.officepsychotherapist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfficePsychotherapistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
